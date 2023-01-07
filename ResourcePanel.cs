7c30130afdb07457cd4dc27dec3ab1af55d705cd
Gabby Delforge <gabrielle.delforge@gmail.com>
2023-01-07 17:45:26 -0500

    basic UI layout but it kinda sucks rn

--- ResourcePanel.cs
+++ ResourcePanel.cs
@@ -1,17 +1,17 @@
 using Godot;
 
-public partial class ResourcePanel : Panel {
+public partial class ResourcePanel : VBoxContainer {
 
   Label twigLabel;
   Label meatLabel;
 
   public override void _Ready() {
-	twigLabel = GetNode<Label>("TwigLabel");
-	meatLabel = GetNode<Label>("MeatLabel");
+  twigLabel = GetNode<Label>("TwigContainer/TwigLabel");
+  meatLabel = GetNode<Label>("MeatContainer/MeatLabel");
   }
 
   public override void _Process(double delta) {
-	twigLabel.Text = "Twigs: " + Globals.TwigCount.ToString();
-	meatLabel.Text = "Meat: " + Globals.MeatCount.ToString();
+  twigLabel.Text = Globals.TwigCount.ToString();
+  meatLabel.Text = Globals.MeatCount.ToString();
   }
 }
