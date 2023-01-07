39c91c494d653f6a911a47cf5dfd60840d07dca4
johnfn <johnfn@gmail.com>
2023-01-06 20:15:49 -0700

    mf

--- ClickableArea2D.gd
+++ ClickableArea2D.gd
@@ -1,14 +1,22 @@
 extends Area2D
 
+var is_mouse_inside = false
+
 func _ready():
   # Add custom signal named mouse_button_pressed
   self.add_user_signal("mouse_button_pressed")
 
-  connect("input_event", func (viewport, input_event, shape_idx):
-    if (
-      input_event is InputEventMouseButton and 
-      input_event.is_pressed() and 
-      input_event.get_button_index() == MOUSE_BUTTON_LEFT):
-
-      self.emit_signal("mouse_button_pressed", input_event.get_position())
+  connect("mouse_entered", func():
+    is_mouse_inside = true
   )
+
+  connect("mouse_exited", func():
+    is_mouse_inside = false
+  )
+
+func _input(event):
+  if event is InputEventMouseButton and event.is_pressed() and event.get_button_index() == MOUSE_BUTTON_LEFT:
+    if is_mouse_inside:
+      get_tree().get_root().set_input_as_handled()
+
+      self.emit_signal("mouse_button_pressed", event.get_position())
--- Map.gd
+++ Map.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-  pass # Replace with function body.
+func _unhandled_input(event):
+  if event is InputEventMouseButton:
+    if event.is_pressed():
+      print("OK (Unhandled)")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
