7f9a4dc0f84e9f86b135848612b7a1659d014936
Gabby Delforge <gabrielle.delforge@gmail.com>
2023-01-07 16:26:22 -0500

    add theme and non functional resource counters

--- UiPanel.cs
+++ UiPanel.cs
@@ -25,189 +25,189 @@ public partial class UiPanel : Panel {
   Button townHallBuyGrasshopperButton;
 
   public override void _Ready() {
-    townHallPanel = GetNode<Panel>("TownHallPanel");
-    buildPanel = GetNode<Panel>("BuildPanel");
-    unitPanel = GetNode<Panel>("UnitPanel");
-    progressLabel = GetNode<Label>("BuildPanel/ProgressLabel");
-    statsLabel = GetNode<Label>("UnitPanel/StatsLabel");
-    selectedUnitLabel = GetNode<Label>("UnitPanel/SelectedUnitLabel");
-    builderPanel = GetNode<Panel>("BuilderPanel");
-    builderPanelLabel = GetNode<Label>("BuilderPanel/SelectedUnitLabel");
-    townHallButton = GetNode<Button>("BuilderPanel/TownHallButton");
-    townHallBuyGrasshopperButton = GetNode<Button>("TownHallPanel/BuyGrasshopper");
+	townHallPanel = GetNode<Panel>("TownHallPanel");
+	buildPanel = GetNode<Panel>("BuildPanel");
+	unitPanel = GetNode<Panel>("UnitPanel");
+	progressLabel = GetNode<Label>("BuildPanel/ProgressLabel");
+	statsLabel = GetNode<Label>("UnitPanel/StatsLabel");
+	selectedUnitLabel = GetNode<Label>("UnitPanel/SelectedUnitLabel");
+	builderPanel = GetNode<Panel>("BuilderPanel");
+	builderPanelLabel = GetNode<Label>("BuilderPanel/SelectedUnitLabel");
+	townHallButton = GetNode<Button>("BuilderPanel/TownHallButton");
+	townHallBuyGrasshopperButton = GetNode<Button>("TownHallPanel/BuyGrasshopper");
 
-    showCommandUi();
+	showCommandUi();
 
-    townHallBuyGrasshopperButton.Connect("pressed", Callable.From(() => {
-      if (selectedUnit is TownHall th) {
-        th.BuyUnit(UnitType.Ant);
-      }
-    }));
+	townHallBuyGrasshopperButton.Connect("pressed", Callable.From(() => {
+	  if (selectedUnit is TownHall th) {
+		th.BuyUnit(UnitType.Ant);
+	  }
+	}));
 
-    townHallButton.Connect("pressed", Callable.From(() => {
-      selectedBuilding = GD.Load<PackedScene>("res://scenes/town_hall.tscn").Instantiate<Sprite2D>();
+	townHallButton.Connect("pressed", Callable.From(() => {
+	  selectedBuilding = GD.Load<PackedScene>("res://scenes/town_hall.tscn").Instantiate<Sprite2D>();
 
-      selectedBuilding.Modulate = new Color(1, 1, 1, 0.5f);
+	  selectedBuilding.Modulate = new Color(1, 1, 1, 0.5f);
 
-      GetNode("/root/Root").AddChild(selectedBuilding);
-      gameMode = GameMode.Build;
-    }));
+	  GetNode("/root/Root").AddChild(selectedBuilding);
+	  gameMode = GameMode.Build;
+	}));
   }
 
   private void showCommandUi() {
-    var townHallPanelVisible = false;
-    var buildPanelVisible = false;
-    var unitPanelVisible = false;
-    var builderPanelVisible = false;
+	var townHallPanelVisible = false;
+	var buildPanelVisible = false;
+	var unitPanelVisible = false;
+	var builderPanelVisible = false;
 
-    if (selectedUnit == null) {
-      selectedUnitLabel.Text = "Selected Unit: None";
-    }
+	if (selectedUnit == null) {
+	  selectedUnitLabel.Text = "Selected Unit: None";
+	}
 
-    // if (selectedUnit is IUnit u) {
-    //   unitPanelVisible = true;
-    //   selectedUnitLabel.Text = "Selected Unit: " + u.unitName;
-    //   selectedUnitName = u.unitName;
-    // }
+	// if (selectedUnit is IUnit u) {
+	//   unitPanelVisible = true;
+	//   selectedUnitLabel.Text = "Selected Unit: " + u.unitName;
+	//   selectedUnitName = u.unitName;
+	// }
 
-    if (selectedUnit is TownHall th) {
-      if (th.status == BuildingStatus.Building) {
-        buildPanelVisible = true;
-      } else {
-        townHallPanelVisible = true;
-      }
-    }
+	if (selectedUnit is TownHall th) {
+	  if (th.status == BuildingStatus.Building) {
+		buildPanelVisible = true;
+	  } else {
+		townHallPanelVisible = true;
+	  }
+	}
 
-    if (selectedUnit is Ant a) {
-      builderPanelVisible = true;
-      builderPanelLabel.Text = "Selected Unit: " + a.unitName;
+	if (selectedUnit is Ant a) {
+	  builderPanelVisible = true;
+	  builderPanelLabel.Text = "Selected Unit: " + a.unitName;
 
-      if (a.InventoryItem != null) {
-        builderPanelLabel.Text += " - Holding " + a.InventoryItem.resourceType;
-      }
-    }
+	  if (a.InventoryItem != null) {
+		builderPanelLabel.Text += " - Holding " + a.InventoryItem.resourceType;
+	  }
+	}
 
-    townHallPanel.Visible = townHallPanelVisible;
-    buildPanel.Visible = buildPanelVisible;
-    unitPanel.Visible = unitPanelVisible;
-    builderPanel.Visible = builderPanelVisible;
+	townHallPanel.Visible = townHallPanelVisible;
+	buildPanel.Visible = buildPanelVisible;
+	unitPanel.Visible = unitPanelVisible;
+	builderPanel.Visible = builderPanelVisible;
   }
 
   public override void _Input(InputEvent @event) {
-    base._Input(@event);
+	base._Input(@event);
 
-    if (@event is InputEventMouseButton mouseEvent) {
-      _handleMouseDown(mouseEvent);
-    }
+	if (@event is InputEventMouseButton mouseEvent) {
+	  _handleMouseDown(mouseEvent);
+	}
 
-    if (@event is InputEventMouseMotion mouseMotionEvent) {
-      _handleMouseMove(mouseMotionEvent);
-    }
+	if (@event is InputEventMouseMotion mouseMotionEvent) {
+	  _handleMouseMove(mouseMotionEvent);
+	}
   }
 
   private void _handleMouseDown(InputEventMouseButton mouseEvent) {
-    if (!mouseEvent.Pressed) {
-      return;
-    }
+	if (!mouseEvent.Pressed) {
+	  return;
+	}
 
-    if (gameMode == GameMode.Build) {
-      if (mouseEvent.ButtonIndex == MouseButton.Left) {
-        selectedBuilding.Modulate = new Color(1, 1, 1, 1);
-        selectedBuilding = null;
-        gameMode = GameMode.Command;
+	if (gameMode == GameMode.Build) {
+	  if (mouseEvent.ButtonIndex == MouseButton.Left) {
+		selectedBuilding.Modulate = new Color(1, 1, 1, 1);
+		selectedBuilding = null;
+		gameMode = GameMode.Command;
 
-        return;
-      }
-    }
+		return;
+	  }
+	}
 
-    if (gameMode == GameMode.Command) {
-      // Issue action like Move
-      if (mouseEvent.ButtonIndex == MouseButton.Right) {
-        if (selectedUnit == null) {
-          return;
-        }
+	if (gameMode == GameMode.Command) {
+	  // Issue action like Move
+	  if (mouseEvent.ButtonIndex == MouseButton.Right) {
+		if (selectedUnit == null) {
+		  return;
+		}
 
-        var hoveredUnit = GetHoveredUnit();
+		var hoveredUnit = GetHoveredUnit();
 
-        if (hoveredUnit is IResource resource) {
-          if (selectedUnit is Ant a) {
-            a.Harvest(resource);
-          }
-        } else {
-          if (selectedUnit is Ant a) {
-            a.Move(Util.MousePosition(GetTree()));
-          }
-        }
+		if (hoveredUnit is IResource resource) {
+		  if (selectedUnit is Ant a) {
+			a.Harvest(resource);
+		  }
+		} else {
+		  if (selectedUnit is Ant a) {
+			a.Move(Util.MousePosition(GetTree()));
+		  }
+		}
 
-        return;
-      }
+		return;
+	  }
 
-      // Select
-      if (mouseEvent.ButtonIndex == MouseButton.Left) {
-        var unit = GetHoveredUnit();
+	  // Select
+	  if (mouseEvent.ButtonIndex == MouseButton.Left) {
+		var unit = GetHoveredUnit();
 
-        if (unit is ISelectable s) {
-          selectedUnit = s;
-          showCommandUi();
-        }
-      }
-    }
+		if (unit is ISelectable s) {
+		  selectedUnit = s;
+		  showCommandUi();
+		}
+	  }
+	}
   }
 
   private void _handleMouseMove(InputEventMouseMotion mouseMotionEvent) {
-    if (gameMode == GameMode.Build) {
-      var pos = Util.MousePosition(GetTree());
-      var roundedTo32 = new Vector2((int)(pos.x / 32) * 32, (int)(pos.y / 32) * 32);
+	if (gameMode == GameMode.Build) {
+	  var pos = Util.MousePosition(GetTree());
+	  var roundedTo32 = new Vector2((int)(pos.x / 32) * 32, (int)(pos.y / 32) * 32);
 
-      selectedBuilding.Position = roundedTo32;
-    }
+	  selectedBuilding.Position = roundedTo32;
+	}
 
-    if (gameMode == GameMode.Command) {
-      var prevHoveredUnit = hoveredUnit;
+	if (gameMode == GameMode.Command) {
+	  var prevHoveredUnit = hoveredUnit;
 
-      hoveredUnit = GetHoveredUnit();
+	  hoveredUnit = GetHoveredUnit();
 
-      if (prevHoveredUnit != hoveredUnit) {
-        if (prevHoveredUnit != null) {
-          prevHoveredUnit.OnHoverEnd();
-        }
+	  if (prevHoveredUnit != hoveredUnit) {
+		if (prevHoveredUnit != null) {
+		  prevHoveredUnit.OnHoverEnd();
+		}
 
-        if (hoveredUnit != null) {
-          hoveredUnit.OnHoverStart();
-        }
-      }
-    }
+		if (hoveredUnit != null) {
+		  hoveredUnit.OnHoverStart();
+		}
+	  }
+	}
   }
 
   public ISelectable GetHoveredUnit() {
-    var mousePosition = Util.MousePosition(GetTree());
-    var param = new PhysicsPointQueryParameters2D();
+	var mousePosition = Util.MousePosition(GetTree());
+	var param = new PhysicsPointQueryParameters2D();
 
-    param.Exclude = new Godot.Collections.Array<RID>();
-    param.CollideWithAreas = true;
-    param.CollideWithBodies = true;
-    param.Position = mousePosition;
+	param.Exclude = new Godot.Collections.Array<RID>();
+	param.CollideWithAreas = true;
+	param.CollideWithBodies = true;
+	param.Position = mousePosition;
 
-    List<Node2D> collisions = GetWorld2d().DirectSpaceState.IntersectPoint(param)
-      .Select(x => (Node2D)x["collider"])
-      .ToList();
+	List<Node2D> collisions = GetWorld2d().DirectSpaceState.IntersectPoint(param)
+	  .Select(x => (Node2D)x["collider"])
+	  .ToList();
 
-    ISelectable best = null;
+	ISelectable best = null;
 
-    // TODO: Maybe handle priority or something
+	// TODO: Maybe handle priority or something
 
-    foreach (var collision in collisions) {
-      if (collision.GetParent() is ISelectable c) {
-        best = c;
-      }
-    }
+	foreach (var collision in collisions) {
+	  if (collision.GetParent() is ISelectable c) {
+		best = c;
+	  }
+	}
 
-    return best;
+	return best;
   }
 
   public override void _Process(double delta) {
-    if (gameMode == GameMode.Command) {
-      showCommandUi();
-    }
+	if (gameMode == GameMode.Command) {
+	  showCommandUi();
+	}
   }
 }
